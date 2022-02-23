69648d7de1ac25e78d407853d6f26e0b14c46d40
iorisama <gilrayga@gmail.com>
2022-02-23 14:25:44 +0700

    Multiplayer Phase 1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if(isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+            transform.position=transform.position + movement;
+        }
+    }    
+    void Update(){
+        HandleMovement();
+    }
+}
